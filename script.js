8ad12dd404b8a1705d5983abe16f6df201fe42ec
Matheus <matheusuon.almeida@eaportal.org>
2024-03-11 12:38:20 -0300

    criacao da navegacao do desktop, remocao da classe do mobile e inicio da responsitividade

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 const BotaoAbrir = document.querySelector(".header-mobile > button")
 const BotaoFechar = document.querySelector(".header-mobile nav button")
+const BotaoAbrir = document.querySelector("header > button")
+const BotaoFechar = document.querySelector("header nav button")
 
 const body = document.querySelector("body")
 const nav = document.querySelector(".header-mobile nav")
+const nav = document.querySelector("header.mobile")
 
 BotaoAbrir.addEventListener('click', abrirMenu)
 BotaoFechar.addEventListener('click', fecharMenu)
-
 function abrirMenu() {
     body.classList.add("escurecer")
     nav.classList.add("abrir")
 }
-
 function fecharMenu() {
     body.classList.remove("escurecer")
     nav.classList.remove("abrir")
